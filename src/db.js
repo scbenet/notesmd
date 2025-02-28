524eb23d6e76d2a5f2cca19539f1528ac223a372
Sam Benet <scbenet@gmail.com>
2025-02-28 15:55:25 -0500

    test pre-commit hook

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
-import Dexie from 'dexie';
+// import Dexie from 'dexie';
 
-export const db = new Dexie('notesDatabase');
-db.version(1).stores({
-    notes: '++id, title, date, '
-})
+// export const db = new Dexie('notesDatabase');
+// db.version(1).stores({
+//     notes: '++id, title, date, '
+// })
